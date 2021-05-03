5fc8de6c4e7ee88f77c2ba5595268f40ab2b59e9
Mustafa Parlaktuna <mparlaktuna@gmail.com>
2021-05-02 22:13:51 -0400

    creating to loop to pass through years and months

---
+++ problem19.lisp
@@ -0,0 +1,34 @@
+;; You are given the following information, but you may prefer to do some research for yourself.
+
+;;     1 Jan 1900 was a Monday.
+;;     Thirty days has September,
+;;     April, June and November.
+;;     All the rest have thirty-one,
+;;     Saving February alone,
+;;     Which has twenty-eight, rain or shine.
+;;     And on leap years, twenty-nine.
+;;     A leap year occurs on any year evenly divisible by 4, but not on a century unless it is divisible by 400.
+
+;; How many Sundays fell on the first of the month during the twentieth century (1 Jan 1901 to 31 Dec 2000)?
+
+;; 9 4 6 11 ->30
+;; 1 3 5 7 8 10 12 ->31
+;; 2 -> 28 (29)
+(use-package :iter)
+
+(defun get-days-in-month (month year)
+  (cond
+    ((member month '(4 6 9 11)) 30)
+    ((member month '(1 3 5 7 8 10 12)) 31)
+    (t (if (= 0 (rem year 4))
+	   29
+	   28))))
+
+
+;; 0 is mondat 1 Jan 1990
+(defun is-sunday (day-number)
+  (eq 6 (rem day-number 7)))
+
+(iter (for year from 1900 to 2000)
+  (iter (for month from 1 to 12)
+    )
